487080678ac68462b47c8cfb6f6c53939772a475
XtratoS <mamdouh.waleed.96@gmail.com>
2022-11-04 16:09:34 +0200

    finalize main

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,41 @@
 require('dotenv').config({ path: __dirname+'/Secret.env' });
 import { EventEmitter } from 'events';
-import { getGroups } from './GroupHandler';
-import { getLastNReplaysAfter, getLatestReplay } from './ReplayHandler';
+import { findOrCreateGroup } from './GroupHandler';
+import { getLastNReplaysAfter, getLatestReplay, setReplayGroup } from './ReplayHandler';
 import { Replay } from './types';
 import { sleep, splitReplayTitle } from './util';
+const parentGroups: {groupId: string, letters: string[]}[] = require('./MatchesParentGroups.json');
 
 const NEW_REPLAY_EVENT = 'new_replay';
-
 const emitter = new EventEmitter();
-
-const getExistingGroups = async () => {
-	const parentGroupId = process.env.BALLCHASING_CURRENT_PARENT_GROUP_ID;
-	if (!parentGroupId) return;
-	const childGroups = await getGroups(parentGroupId);
-	return childGroups;
-}
 
 const handleNewReplay = async (replay: Replay) => {
 	const replayData = splitReplayTitle(replay);
-	const replayGroupsPromise = getExistingGroups();
+
+  if (!replayData) return;
+  if (replayData.region.toLowerCase() !== 'mena') return;
+  if (replayData.gameIndex === '0') return;
+
+  const parentGroupId = parentGroups.find((groupEntry) => groupEntry.letters.includes(replayData.seriesLetter))?.groupId;
+  if (!parentGroupId) return;
+
+  const groupName = replayData.team1abbr.toUpperCase() + ' vs ' + replayData.team2abbr.toUpperCase();
+  const groupId = await findOrCreateGroup(parentGroupId, groupName);
+  if (groupId === '') return;
+
+  console.log('adding new replay ' + replay.replay_title + ' to ' + groupName);
+  const res = await setReplayGroup(replay.id, groupId);
 }
 
 emitter.on(NEW_REPLAY_EVENT, handleNewReplay);
 
 const pollAndAssign = async (latestReplay: Replay) => {
 	await sleep(5000);
-	console.log('Polling for new replays!', latestReplay.created);
 	const newlyUploaded = await getLastNReplaysAfter(20, new Date(+new Date(latestReplay.created)+1000).toISOString());
 	if (newlyUploaded.length > 0) {
 		latestReplay = newlyUploaded[0];
 	}
-	console.log(newlyUploaded);
 	newlyUploaded.forEach(replay => {
-		console.log(latestReplay.created, replay.created);
-		console.log('new replay!');
 		emitter.emit(NEW_REPLAY_EVENT, replay);
 	});
 	pollAndAssign(latestReplay);
