e792517eeaa0bf67501d76f78c769ae8fff5e01c
XtratoS <mamdouh.waleed.96@gmail.com>
2022-11-04 16:08:40 +0200

    set replay group function

--- src/ReplayHandler.ts
+++ src/ReplayHandler.ts
@@ -1,17 +1,14 @@
 import { makeAPIRequest } from "./API";
 import { Replay } from "./types";
 
-export const getLastNReplays = async (n = 20): Promise<Replay[]> => {
-	const res = await makeAPIRequest(`replays?uploader=me&count=${n}`, 'GET', null);
-	if (!res) {
-		return [];
-	}
-	const json = await res.json();
-	return json.list;
+export const setReplayGroup = async (replayId: string, groupId: string) => {
+  const res = await makeAPIRequest(`replays/${replayId}`, 'PATCH', {
+    group: groupId
+  });
 }
 
 export const getLastNReplaysAfter = async (n = 20, after: string): Promise<Replay[]> => {
-	const res = await makeAPIRequest(`replays?uploader=me&count=${n}&created-after=${after}`, 'GET', null);
+	const res = await makeAPIRequest(`replays?uploader=me&count=${n}&created-after=${after}&title=mena`, 'GET', null);
 	if (!res) {
 		return [];
 	}
@@ -20,17 +17,10 @@ export const getLastNReplaysAfter = async (n = 20, after: string): Promise<Repla
 }
 
 export const getLatestReplay = async (): Promise<Replay|undefined> => {
-	const res = await makeAPIRequest(`replays?uploader=me&count=1`, 'GET', null);
+	const res = await makeAPIRequest(`replays?uploader=me&count=1&title=mena`, 'GET', null);
 	if (!res) {
 		return undefined;
 	}
 	const json = await res.json();
 	return json.list[0];
-}
-
-export const extractNewlyUploadedFromPolled = (latestReplay: Replay, polledReplays: Replay[] = []) => {
-	const latestReplayDate = latestReplay.date;
-	const latestReplayTimestamp = +new Date(latestReplayDate);
-	const newlyUploadedReplays = polledReplays.filter(replay => +new Date(replay.date) > latestReplayTimestamp);
-	return newlyUploadedReplays;
 }
